a23bcd57ce149d41d1eb4e9fa89d9e52a5f9d162
kosko-mj <252770322+kosko-mj@users.noreply.github.com>
2026-02-22 11:21:53 -0500

    feat(library): Add array and addBookToLibrary function with unique IDs

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 // Book Constructor
 function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    // Info Method
-    this.info = function() {
-        let readIcon = this.read ? '<i class="ri-checkbox-circle-line"></i>' : '<i class="ri-book-open-line"></i>';
-        let readText = this.read ? 'read' : 'not read yet';
-        
-        return `${this.title} by ${this.author} - ${this.pages} pages - ${readIcon} ${readText}`;
-    };
 }
 
-// Books
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const book2 = new Book("Dune", "Frank Herbert", 412, true);
+// Info Method
+Book.prototype.info = function() {
+    let readIcon = this.read ? '<i class="ri-checkbox-circle-line"></i>' : '<i class="ri-book-open-line"></i>';
+    let readText = this.read ? 'read' : 'not read yet';
+        
+    return `${this.title} by ${this.author} - ${this.pages} pages - ${readIcon} ${readText}`;
+};
 
-// Test
-console.log(book1.info());
-console.log(book2.info());
+// Array to Store Books
+const myLibrary = [];
+
+// Function to Add Books to Library
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayBooks();
+}
+
+// Function to Display Books on Page
+function displayBooks() {
+
+}
+
+// Test Books
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, true);
+
+console.log(myLibrary);
+
